537036e1d4c3df0381d945a461e1527f2eebfb90
Rayyan365 <rayanabdallah09@gamil.com>
2023-07-25 23:12:24 -0700

    calctip2 done

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+function calctip(){
+    var tip;
+    if (bill>=50 && bill<=300){
+        tip= (15/100)*bill
+    }
+    if (bill>300){
+        tip= (20/100)*80
+    }
+    return tip;
+}
